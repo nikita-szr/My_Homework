73c09788c85bb070166a3b22c7fe20612020b800
Nikita Osipov <Nikittaosipov@yandex.ru>
2024-05-12 18:43:14 +0400

    pytest modules added

---
+++ tests/test_masks.py
@@ -0,0 +1,31 @@
+from src.masks import mask_card_number, mask_bank_account
+import pytest
+
+
+@pytest.fixture
+def testing_card_numbers():
+    return [1234567812345678, 1234, "12345678123456781234567812345678", 7000792289606361]
+
+
+def test_mask_card_number(testing_card_numbers):
+    assert mask_card_number(testing_card_numbers[0]) == "1234 56XX XXXX 5678"
+    assert mask_card_number(testing_card_numbers[1]) == "Неверный номер карты"
+    assert mask_card_number(testing_card_numbers[2]) == "Неверный номер карты"
+    assert mask_card_number(testing_card_numbers[3]) == "7000 79XX XXXX 6361"
+
+
+@pytest.fixture
+def testing_bank_accounts():
+    return [48126790351872493601, 69540218372591084732, 82107364928504617395]
+
+
+def test_mask_bank_account(testing_bank_accounts):
+    assert mask_bank_account(testing_bank_accounts[0]) == "**3601"
+    assert mask_bank_account(testing_bank_accounts[1]) == "**4732"
+    assert mask_bank_account(testing_bank_accounts[2]) == "**7395"
+
+
+
+
+
+
---
+++ tests/test_widget.py
@@ -0,0 +1,30 @@
+from src.widget import card_or_account_mask, convert_date
+import pytest
+
+
+@pytest.fixture
+def testing_card_or_account_mask():
+    return [
+        ("счет 1234567812345678", "Неверный номер счёта"),
+        ("счет 12345678123456781234567812345678", "**5678"),
+        ("карта 1234567812345678", "карта 1234 56XX XXXX 5678"),
+        ("карта 1234567812345678", "карта 1234 56XX XXXX 5678"),
+        ("некорректные данные", "Неверный номер карты")
+    ]
+
+
+def test_card_or_account_mask(testing_card_or_account_mask):
+    for numbers, expecting_mask in testing_card_or_account_mask:
+        assert card_or_account_mask(numbers) == expecting_mask
+
+
+@pytest.mark.parametrize("input_date, expected_output", [
+    ("2022-05-01T00:00:00", "01.05.2022"),
+    ("2022-12-31T00:00:00", "31.12.2022"),
+    ("2023-02-30T00:00:00", "30.02.2023"),
+    ("2023-06-15T00:00:00", "15.06.2023")
+])
+
+
+def test_convert_date(input_date, expected_output):
+    assert convert_date(input_date) == expected_output
