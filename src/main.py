857f3d25c980e5e3a473456cec8096e0753d2dc2
Nikita Osipov <Nikittaosipov@yandex.ru>
2024-05-12 18:51:11 +0400

    very small fix main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.widget import card_or_account_mask, convert_date
 
 masked_card_number = mask_card_number(7000792289606361)
 masked_bank_account = mask_bank_account(73654108430135874305)
-masked_card_or_account = card_or_account_mask("карта 1234567812345678")
+masked_card_or_account = card_or_account_mask("Maestro 7000792289606361")
 converted_date = convert_date("2018-07-11T02:26:18.671407")
 
 print(masked_card_number)
