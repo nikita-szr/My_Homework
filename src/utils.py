16f73b3f26d51cb9d553dda5f4a4607d46840d24
Nikita Osipov <Nikittaosipov@yandex.ru>
2024-06-09 19:51:06 +0400

    log file to module utils

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,16 @@
 import json
+import logging
 from typing import Any, Dict, List, Union
 
 from src.external_api import currency_conversion
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('../logs/utils.log', mode='w')
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def json_transactions_data(path: str) -> List[Dict[str, Any]]:
@@ -10,16 +19,21 @@ def json_transactions_data(path: str) -> List[Dict[str, Any]]:
         with open(path, 'r', encoding='utf-8') as file:
             transactions = json.load(file)
             if transactions:
+                logger.info(f"json файл перекодирован в список словарей: {json_transactions_data}")
                 return transactions
             else:
+                logger.info(f"в json файле нет словарей: {json_transactions_data}")
                 return []
     except FileNotFoundError:
+        logger.error(f"Файл не найден: {json_transactions_data}")
         print("Ошибка: Файл не найден.")
         return []
     except json.JSONDecodeError:
+        logger.error(f"Файл не является json объектом: {json_transactions_data}")
         print("Ошибка: Файл не является json объектом.")
         return []
     except Exception as e:
+        logger.error(f"Ошибка{e}: {json_transactions_data}")
         print(f"Ошибка: {e}")
         return []
 
@@ -32,12 +46,15 @@ def transaction_amount(transactions: List[Dict[str, Any]]) -> Union[float, None]
             currency_code = transaction["operationAmount"]["currency"]["code"]
             if currency_code == "RUB":
                 amount = float(transaction["operationAmount"]["amount"])
+                logger.info(f"Определена сумма транзакции в рублях: {json_transactions_data}")
                 return amount
             else:
                 amount_: float | None = currency_conversion(currency_code, "RUB",
                                                             float(transaction["operationAmount"]["amount"]))
+                logger.info(f"Сумма транзакции конвертирована в рубли: {json_transactions_data}")
                 return amount_
         else:
+            logger.error(f"Некорректный формат данных транзакции: {json_transactions_data}")
             print("Ошибка: Некорректный формат данных транзакции.")
             result = None
     return result
